4599c7101b091303306ed4edaa61b2e5dcfb026a
koad7 <kodjo.adanledji@gmail.com>
2021-08-17 23:21:44 +0200

    pre deploy

--- core/settings.py
+++ core/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['https://dsa-gatf.herokuapp.com/']
+ALLOWED_HOSTS = ['dsa-gatf.herokuapp.com']
 
 # Application definition
 
