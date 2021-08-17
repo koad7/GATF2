7a339baf5a1660309887774710935c8fee6d77f3
koad7 <kodjo.adanledji@gmail.com>
2021-08-17 23:18:36 +0200

    pre deploy

--- backend/views.py
+++ backend/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.views.generic import View
-from django.conf import settings 
+from django.conf import settings
 from django.http import HttpResponse
 import os
 import logging
@@ -9,31 +9,33 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from backend.get_airtable_data import airtable_func
 
+
 def index(request):
     return render(request, "build/index.html")
+
 
 class FrontendAppView(View):
     """
     Serves the compiled frontend entry point (only works if you have run `yarn
     run build`).
     """
-
     def get(self, request):
-            print (os.path.join(settings.REACT_APP_DIR, 'build', 'index.html'))
-            try:
-                with open(os.path.join(settings.REACT_APP_DIR, 'build', 'index.html')) as f:
-                    return HttpResponse(f.read())
-            except FileNotFoundError:
-                logging.exception('Production build of app not found')
-                return HttpResponse(
-                    """
+        print(os.path.join(settings.REACT_APP_DIR, 'build', 'index.html'))
+        try:
+            with open(
+                    os.path.join(settings.REACT_APP_DIR, 'build',
+                                 'index.html')) as f:
+                return HttpResponse(f.read())
+        except FileNotFoundError:
+            logging.exception('Production build of app not found')
+            return HttpResponse(
+                """
                     This URL is only used when you have built the production
                     version of the app. Visit http://localhost:3000/ instead, or
                     run `yarn run build` to test the production version.
                     """,
-                    status=501,
-                )
-
+                status=501,
+            )
 
 
 class AiratableView(APIView):
--- core/settings.py
+++ core/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['https://dsa-gatf.herokuapp.com/']
 
 # Application definition
 
