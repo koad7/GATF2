a6e029030637939769b2cb13961c13275aef9338
koad7 <kodjo.adanledji@gmail.com>
2022-01-17 22:02:56 +0100

    Merge dev to main

--- core/settings.py
+++ core/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 import os
 from dotenv import load_dotenv
+import mimetypes
+
+mimetypes.add_type("text/javascript", ".js", True)
 
 load_dotenv()
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
