fbacdc4a03baa5c842a79e7f0e65465a5fadea8c
koad7 <kodjo.adanledji@gmail.com>
2022-02-09 23:15:38 +0100

    Map

--- frontend/src/components/Dashboard/components/Map.js
+++ frontend/src/components/Dashboard/components/Map.js
@@ -1,9 +1,13 @@
 import React from 'react';
-import Highcharts from "highcharts";
 import { renderToStaticMarkup } from 'react-dom/server';
 
+// Import Highcharts
+import Highcharts from "highcharts";
+import HighchartsMap from "highcharts/modules/map";
 import HighchartsReact from "highcharts-react-official";
-import highchartsMap from "highcharts/modules/map";
+
+import map from "@highcharts/map-collection/custom/world.geo";
+
 import proj4 from "proj4";
 
 import { makeStyles } from '@material-ui/core/styles';
@@ -62,132 +66,98 @@ function MapTooltipCard(props) {
 
 
 
-highchartsMap(Highcharts);
+HighchartsMap(Highcharts);
 
-const mapDataIE = require('@highcharts/map-collection/custom/world.geo.json');
 
 if (typeof window !== "undefined") {
     window.proj4 = window.proj4 || proj4;
 }
 
-  
-function createOption(data){
-  const mapOptions = {
-    chart: {
-        map: mapDataIE,
-        borderWidth: 0
-    },
-    title: {
-      text: ''
-    },
-    credits: {
-        enabled: false
-    },
-    legend:{
-      enabled: false
-    },
-    borderColor: "#343a40",
-    mapNavigation: {
-      enabled: true,
-      enableDoubleClickZoomTo: true,
-      buttonOptions: {
-          theme: {
-              fill: '#eead2200',
-              'stroke-width': 0,
-              stroke: '#eead2200',
-              r: 0,
-              states: {
-                  hover: {
-                      fill: '#eead2200'
-                  },
-                  select: {
-                      stroke: '#eead2200',
-                      fill: '#eead2200'
-                  }
-              }
-          },
-          verticalAlign: 'bottom'
-      },
-      buttons:{
-        zoomIn: {
-          text: ""
-        },
-        zoomOut: {
-          text: ""
-        }
-      }
-    },
-
-    tooltip: {
-      stickOnContact: true,
-      stickyTracking:false,
-
-
-      useHTML: true,
-        positioner: function () {
-          return { x: 0, y: 0 };
-      },
-      formatter: function(){
-        return  renderToStaticMarkup(<MapTooltipCard name={this.point.name} budget={this.point.options['value']} project={this.point.options['Project']} inkind={this.point.options['In-kind Estimation']}/>)
-      },
-     
-      style: {
-        width: "500px",
-      },
-    },
-    plotOptions: {
-      series: {
-        point: {
-          events: {
-            click: function () {
-              // Modal creation
-              // let modal = document.getElementById("map-width-modal");
-              // const close = document.querySelector('[data-dismiss="modal"]');
-              // close.addEventListener(
-              //   "click",
-              //   () => (modal.style.display = "none")
-              // );
-              // let countrydata = CountryTablejson(this.options);
-              // createCountryTable(countrydata); // Build Table
-              // countrychart(countrydata); // Draw map
-              // modal.style.display = "block";
-              // modal.style.opacity = 1;
-              // updatecountrycard(this.region, this.name, this.code3);
-              return this.y +
-          '<br/>Some text.<br/><a href="http://www.highcharts.com"/>Click here to read more</a>'
-            },
-          },
-        },
-      },
-    },
-    
-  series: [{
-    name: 'Countries',
-    color: '#E0E0E0',
-    enableMouseTracking: false
-},{
-  // type: 'mapbubble',
-    borderWidth:0,
-    borderColor: '#a8a8a8',
-    nullColor: '#a8a8a8',
-    joinBy: ['hc-key'],
-    data: data,
-    minSize: 9,
-    maxSize: 9,
-    cursor: 'pointer' ,
-    tooltip: {
-              pointFormat: 'yep {point.In-kind Estimation}: {point.Project} '
-          }
-  }]
-  }
-  return mapOptions
-}
 export default  function Map(props) {
+
+  
+    const mapOptions = {
+      chart: {
+          map: map,
+          borderWidth: 0
+      },
+      title: {
+        text: ''
+      },
+      credits: {
+          enabled: false
+      },
+      legend:{
+        enabled: false
+      },
+      // borderColor: "#343a40",
+      mapNavigation: {
+        enabled: true,
+        enableDoubleClickZoomTo: true,
+        buttonOptions: {
+            theme: {
+                fill: '#eead2200',
+                'stroke-width': 0,
+                stroke: '#eead2200',
+                r: 0,
+                states: {
+                    hover: {
+                        fill: '#eead2200'
+                    },
+                    select: {
+                        stroke: '#eead2200',
+                        fill: '#eead2200'
+                    }
+                }
+            },
+            verticalAlign: 'bottom'
+        },
+        buttons:{
+          zoomIn: {
+            text: ""
+          },
+          zoomOut: {
+            text: ""
+          }
+        }
+      },
+  
+      tooltip: {
+        stickOnContact: true,
+        stickyTracking:false,
+        useHTML: true,
+          positioner: function () {
+            return { x: 0, y: 0 };
+        },
+        formatter: function(){
+          return  renderToStaticMarkup(<MapTooltipCard name={this.point.name} budget={this.point.options['z']} project={this.point.options['Project']} inkind={this.point.options['In-kind Estimation']}/>)
+        },
+        style: {
+          width: "500px",
+        },
+      },
+    series: [{
+      name: 'Countries',
+      color: '#E0E0E0',
+      borderWidth:0,
+      borderColor: '#a8a8a8',
+      // nullColor: '#a8a8a8',
+      enableMouseTracking: false
+  },{
+      type: "mapbubble",
+      joinBy: ["iso-a3", "code3"],
+      data: props.mapData,
+      minSize: 9,
+      maxSize: 9
+    }]
+    }
+  
+
     return (<div>
       <HighchartsReact
         constructorType={"mapChart"}
         highcharts={Highcharts}
-        options={createOption(props.mapData)}
+        options={mapOptions}
         containerProps={{ style: { height: 'calc(100vh - 170px)' } }}
       />
     </div>);
