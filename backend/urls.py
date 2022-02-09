0df40194905658e4628038f511fbbfb83a20a0b3
koad7 <kodjo.adanledji@gmail.com>
2022-02-09 22:54:14 +0100

    Change dsa-test

--- backend/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import re_path, path
-from backend.views import FrontendAppView
-from backend.views import AiratableView
-
-urlpatterns = [
-    path('api/', AiratableView.as_view(), name='airtable-view'),
-    re_path('', FrontendAppView.as_view(), name='home'),
-]
